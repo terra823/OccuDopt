d702584a9f211d51071edee1c91d8d717387c6b0
Chris Csikszentmihalyi <robotic@gmail.com>
2011-11-04 17:08:48 -0400

    modified README file

--- occudopt/config.py
+++ occudopt/config.py
@@ -7,7 +7,6 @@ DEBUG = True
 #db.auth("")
 SECRET_KEY = 'csikszentmihalyi'
 MONGODB_DB = 'occudopt'
-#MONGO_USER = 'boss'
-#MONGO_PASS = '1nch4rg3'
+
 # Flask Uploads
 UPLOADS_DEFAULT_DEST = '/Users/csik/Documents/code/flask/OccuDopt/occudopt/uploads'
